84ae13fe11edfbdafc634a97a6fbb8a7d948306e
Lee <liam.eakins@outlook.com>
2018-12-19 04:35:34 -0600

    Write myStory function

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ let creature = prompt("Enter a creature");
 let adjective = prompt("Enter an adjective");
 let verb = prompt("Enter a verb in the past tense");
 
-let story = prompt("Which story would you like? <br> a) Snacking <br> b) Escaping <br> c) Winning");
+
+
+function myStory() {
+    let story = prompt("Which story would you like? <br> a) Snacking <br> b) Escaping <br> c) Winning");
+    if(story == "a") {
+        document.write("After snacking on " + adjective + " treats, the " + color + " bellied " + creature + " " + verb + " for hours.");
+    } else if(story == "b") {
+        document.write("Amelia " + verb + " through the " + adjective + " " + color + " nebula escaping the space " + creature + "."); 
+    } else {
+        document.write("Jackson chose his " + adjective + ", " + color + creature + " card and " + verb + " it to the table knowing he won.");
+    }
+}
+
+myStory();
