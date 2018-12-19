c6cbf167b1a1810c9d8d51cf3afa1874a56e34f5
Lee <liam.eakins@outlook.com>
2018-12-19 03:42:20 -0600

    Declare story variable

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ let creature = prompt("Enter a creature");
 let adjective = prompt("Enter an adjective");
 let verb = prompt("Enter a verb in the past tense");
 
+let story = prompt("Which story would you like? <br> a) Snacking <br> b) Escaping <br> c) Winning");
