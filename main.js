66fda7d4f67a37987ea352e2a95204e0c162b9df
Lee <liam.eakins@outlook.com>
2018-12-19 03:36:13 -0600

    Declare variables

---
+++ main.js
@@ -0,0 +1,5 @@
+let color = prompt("Enter a color:");
+let creature = prompt("Enter a creature");
+let adjective = prompt("Enter an adjective");
+let verb = prompt("Enter a verb in the past tense");
+
